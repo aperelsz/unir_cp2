4024cfb834e04b0bf211f2ec76fb3cbdda4fcd1f
aperelsz <alejoperelsztein@gmail.com>
2023-02-24 20:33:02 -0300

    sku standard

--- terraform/resources_acr.tf
+++ terraform/resources_acr.tf
@@ -7,7 +7,7 @@ resource "azurerm_container_registry" "acr" {
   name                = "containerRegistryUNIR2"
   resource_group_name = azurerm_resource_group.rg_acr.name
   location            = azurerm_resource_group.rg_acr.location
-  sku                 = "Standard"
+  sku                 = "Basic"
 }
 
 
