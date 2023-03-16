fd6b419fe9393aa8ae22f37ba7d2eb8acb042dd5
aperelsz <alejoperelsztein@gmail.com>
2023-03-15 23:10:34 -0300

    playbooks - webserver

--- terraform/credentials.tf
+++ terraform/credentials.tf
@@ -11,3 +11,16 @@ variable "tenant_id" {
   default = "899789dc-202f-44b4-8472-a6d40f9eb440"
   #default = "XX"
 }
+
+variable username {
+  type        = string
+  default     = "azureuser"
+  #default    = "XX"
+  }
+
+variable admin_username {
+  type        = string
+  default     = "azureuser"
+  #default    = "XX"
+  }
+
--- terraform/resources_acr.tf
+++ terraform/resources_acr.tf
@@ -1,20 +1,25 @@
-/*resource "azurerm_resource_group" "rg_acr" {
-  name     = var.resource_group_name_acr
-  location = var.location_name_acr
-}
-
-resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistryUNIR2"
-  resource_group_name = azurerm_resource_group.rg_acr.name
-  location            = azurerm_resource_group.rg_acr.location
-  sku                 = "Basic"
-}*/
 resource "azurerm_container_registry" "acr" {
   name                = "containerRegistryUNIR2"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
+#indica el tipo de identidad que debe configurarse al loguearse en el servicio
+identity {
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.acr_user.id
+    ]
+  }
 }
+
+
+resource "azurerm_user_assigned_identity" "acr_user" {
+  location            = azurerm_resource_group.rg.location
+  name                = var.username
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
 
 
 
