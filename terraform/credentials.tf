c00137fc6876d766ec0d9634a65894c9fa40b060
aperelsz <alejoperelsztein@gmail.com>
2023-03-20 20:59:29 -0300

    actualizacion

--- terraform/credentials.tf
+++ terraform/credentials.tf
@@ -1,26 +1,26 @@
 variable "subscription_id" {
   type = string
   description = "Id de suscripcion"
-  default = "0d29f7a5-92f2-434e-85f8-2f8f610ba718"
-  #default = "XX"
+  #default = "0d29f7a5-92f2-434e-85f8-2f8f610ba718"
+  default = "XX"
 }
 
 variable "tenant_id" {
   type = string
   description = "Id tenant"
-  default = "899789dc-202f-44b4-8472-a6d40f9eb440"
-  #default = "XX"
+  #default = "899789dc-202f-44b4-8472-a6d40f9eb440"
+  default = "XX"
 }
 
 variable username {
   type        = string
-  default     = "azureuser"
-  #default    = "XX"
+  #default     = "azureuser"
+  default    = "XX"
   }
 
 variable admin_username {
   type        = string
-  default     = "azureuser"
-  #default    = "XX"
+  #default     = "azureuser"
+  default    = "XX"
   }
 
